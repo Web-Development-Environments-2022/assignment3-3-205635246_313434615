20ff7afb151e2f29839f19a8dadb6103f9d14215
ShaharWeisberger <81864845+ShaharWeisberger@users.noreply.github.com>
2022-08-19 16:35:28 +0300

    friday-commit-0

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,11 @@ const routes = [
     component: Main,
   },
   {
+    path: "/about",
+    name: "About",
+    component: () => import("./pages/AboutPage"),
+  },
+  {
     path: "/register",
     name: "register",
     component: () => import("./pages/RegisterPage"),
@@ -16,6 +21,26 @@ const routes = [
     path: "/login",
     name: "login",
     component: () => import("./pages/LoginPage"),
+  },
+  {
+    path: "/favorites",//need to change
+    name: "Favorites",
+    component: () => import("./pages/FavoritePage"),
+  },
+  {
+    path: "/getRecipes",//need to change
+    name: "MyRecipes",
+    component: () => import("./pages/MyRecipes"),
+  },
+  {
+    path: "/familyRecipes",//need to change
+    name: "Family",
+    component: () => import("./pages/FamilyPage"),
+  },
+  {
+    path: "/createRecipes",//need to change
+    name: "CreateRecipe",
+    component: () => import("./pages/CreateRecipePage"),
   },
   {
     path: "/search",
@@ -27,6 +52,12 @@ const routes = [
     name: "recipe",
     component: () => import("./pages/RecipeViewPage"),
   },
+  {
+    path: "cusi",
+    name: "cuisine",
+    component: () => import("./assets/cuisine"),
+  },
+  
   {
     path: "*",
     name: "notFound",
